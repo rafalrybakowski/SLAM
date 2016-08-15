1d67853357e17f82300b688d983a6e1394438d65
rafalrybakowski <rafal.rybakowski@slidekamera.pl>
2016-08-15 17:32:40 +0200

    * ADC reflective sensors manager added

---
+++ SLAM/SW4STM32/SLAM/Application/User/motor/MotorManager.h
@@ -0,0 +1,13 @@
+/*
+ * MotorManager.h
+ *
+ *  Created on: Aug 15, 2016
+ *      Author: Rafal
+ */
+
+#ifndef APPLICATION_USER_MOTOR_MOTORMANAGER_H_
+#define APPLICATION_USER_MOTOR_MOTORMANAGER_H_
+
+
+
+#endif /* APPLICATION_USER_MOTOR_MOTORMANAGER_H_ */
---
+++ SLAM/SW4STM32/SLAM/Application/User/reflective/ReflectiveManager.h
@@ -0,0 +1,13 @@
+/*
+ * ReflectiveManager.h
+ *
+ *  Created on: Aug 15, 2016
+ *      Author: Rafal
+ */
+
+#ifndef APPLICATION_USER_REFLECTIVE_REFLECTIVEMANAGER_H_
+#define APPLICATION_USER_REFLECTIVE_REFLECTIVEMANAGER_H_
+
+void reflectiveRead(ADC_HandleTypeDef *hi2c, uint32_t *reading);
+
+#endif /* APPLICATION_USER_REFLECTIVE_REFLECTIVEMANAGER_H_ */
